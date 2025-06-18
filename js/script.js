c86e13b06f170fb9a3db09ea75420b11d0d77621
kxmiii <flewotnesquik@gmail.com>
2025-06-18 22:01:33 +0300

    fix correcting paths

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
 	document.body.classList.add('loaded')
+
+	document.querySelectorAll('a').forEach(link => {
+		if (link.href.includes('.html')) {
+			link.addEventListener('click', e => {
+				e.preventDefault()
+				document.body.classList.remove('loaded')
+
+				setTimeout(() => {
+					window.location.href = link.href
+				}, 300)
+			})
+		}
+	})
 })
